4db762cec15733794a4c80109d6a20e25b792ebb
ViliusVed <vilius.vedegys@gmail.com>
2022-11-11 03:51:51 +0200

    login logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { GoogleApiService } from './google-api.service';
+import { GoogleApiService, UserInfo } from './google-api.service';
 
 @Component({
   selector: 'app-root',
@@ -7,9 +7,39 @@ import { GoogleApiService } from './google-api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Google_login';
+  title = 'google-login-test';
 
-  constructor(private readonly google: GoogleApiService){
-     
+mailSnippets:string[] = []
+  userInfo?: UserInfo
+
+  constructor(private readonly googleApi: GoogleApiService) {
+    googleApi.userProfileSubject.subscribe(info => {
+      this.userInfo = info
+    })
   }
+isLoggedIn():boolean{
+  return this.googleApi.isLoggedIn()
+}
+
+logout(){
+  this.googleApi.singOut()
+}
+
+async getEmails(){
+  if (!this.userInfo){
+    return;
+  }
+
+// const userId = this.userInfo?.info.sub as string
+// const messages = await lastValueFrom(this.googleApi.emails(userId))
+//     messages.messages.forEach( (element: any) => {
+//       const mail = lastValueFrom(this.googleApi.getMail(userId, element.id))
+//       mail.then( mail => {
+//         this.mailSnippets.push(mail.snippet)
+//       })
+//     });
+
+}
+
+
 }
--- src/app/google-api.service.ts
+++ src/app/google-api.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AuthConfig, OAuthService } from 'angular-oauth2-oidc';
+import { Observable, Subject } from 'rxjs';
 
 const oAuthConfig: AuthConfig = {
   issuer: 'https://accounts.google.com',
@@ -9,23 +10,40 @@ const oAuthConfig: AuthConfig = {
   scope: 'openid profile email'
 }
 
+export interface UserInfo {
+  sub: string,
+  email: string,
+  name: string,
+  picture: string,
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class GoogleApiService {
 
+  userProfileSubject = new Subject<UserInfo>()
+
   constructor(private readonly oAuthService: OAuthService) {
     oAuthService.configure(oAuthConfig)
     oAuthService.loadDiscoveryDocument().then(() => {
       oAuthService.tryLoginImplicitFlow().then(() => {
-        if(!oAuthService.hasValidAccessToken()){
+        if (!oAuthService.hasValidAccessToken()) {
           oAuthService.initLoginFlow()
-        } else{
+        } else {
           oAuthService.loadUserProfile().then((userProfile) => {
-            console.log(JSON.stringify(userProfile))
+            this.userProfileSubject.next(userProfile as UserInfo)
           })
         }
       })
     })
   }
+  isLoggedIn(): boolean {
+    return this.oAuthService.hasValidAccessToken()
+  }
+
+singOut(){
+  this.oAuthService.logOut()
+}
+
 } 
